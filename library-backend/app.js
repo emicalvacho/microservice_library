d601e89ba14d91af34b51f22768da9b499f7783c
emicalvacho <emilianocalvacho@gmail.com>
2020-02-19 17:42:44 -0300

    Corrections in Backend

--- library-backend/app.js
+++ library-backend/app.js
@@ -42,8 +42,7 @@ const query = util.promisify(con.query).bind(con);
 app.post('/login', async function (request, response) {
     var username = request.body.username;
     var password = request.body.password;
-    console.log(request.body.username);
-    console.log(password);
+
     const queryAuth = 'SELECT * FROM usuarios WHERE username = ? AND pass = ?';
     var results = await query(queryAuth, [username, password]);
     if (username && password) {
@@ -51,54 +50,66 @@ app.post('/login', async function (request, response) {
             request.session.loggedin = true;
             request.session.id_user = results[0].id;
             request.session.rol = results[0].rol;
-            response.status(200);
-            response.json("User Logged");
+            if(request.session.rol == "b" || request.session.rol == "B"){
+                request.session.admin = true;
+            }
+            response.status(200).json("User Logged");
         } else {
-            response.status(404);
-            response.json("User or Password incorrect");
+            response.status(404).json("Usuario o contraseña incorrecto!");
         }
     } else {
-        response.status(400);
-        response.json('Please enter Username and Password!');
+        response.status(400).json('Por favor ingrese un nombre de usuario o contraseña!');
 
     }
 });
 
+// LOGOUT
+app.post('/logout', function (req, res) {
+    req.session.destroy();
+    res.send("Logout success!");
+    res.end();
+});
+
 // Registro de usuarios (socios, el admin se carga a mano en la BD)
 app.post('/usuarios',async function(req, res){
-    if(req.body.nombre == null){
+    if(!req.body.nombre){
         res.status(400).json("Debe completar el campo nombre");
     }
-    else if(req.body.username == null){
+    else if(!req.body.username){
         res.status(400).json("Debe completar el campo username");
+    }
+    else if(!req.body.password){
+        res.status(400).json("Debe completar el campo password");
+    }
+    else if(!req.body.rol){
+        res.status(400).json("Debe completar el campo rol");
     }
     else if(req.body.username == "admin"){
         res.status(401).json("Este username esta reservado");
     }
-    else if(req.body.password == null){
-        res.status(400).json("Debe completar el campo password");
-    }
-    else if(req.body.rol == null){
-        res.status(400).json("Debe completar el campo rol");
-    }
     else{
-        const queryUser = `select * from usuarios where username = (?)`;
-        var queryUsername = await query(queryUser,req.body.username);
+        if(req.body.rol == "s"  || req.body.rol == "S"){
+            const queryUser = `select * from usuarios where username = (?)`;
+            var queryUsername = await query(queryUser,req.body.username);
 
-        if(queryUsername.length > 0){
-            res.status(400).json("Username ya existente!");
+            if(queryUsername.length > 0){
+                res.status(400).json("Username ya existente!");
+            }
+            else{
+                const queryStr = `insert into usuarios (id,nombre,username,pass,rol) values (?,?,?,?,?)`;   
+                var querySocios = await query(queryStr, [getNewID(), req.body.nombre, req.body.username, req.body.password, req.body.rol]);
+                res.status(200).json("Socio agregado correctamente");
+            }
         }
         else{
-            const queryStr = `insert into usuarios (id,nombre,username,pass,rol) values (?,?,?,?,?)`;   
-            var querySocios = await query(queryStr, [getNewID(), req.body.nombre, req.body.username, req.body.password, req.body.rol]);
-            res.status(200).json("Socio agregado correctamente");
+            res.status(400).json("El campo rol debe contener 's' o 'S'");
         }
     }
 })
 
 // AUTORIZACION (solo bibliotecario)
 var auth = function(req, res, next) {
-    if (req.session && req.session.user === "admin" && req.session.admin)
+    if (req.session && req.session.admin)
         return next();
     return res.sendStatus(401);
 };
@@ -110,24 +121,27 @@ var auth = function(req, res, next) {
 // GET usuarios : listar todos los usuarios cargados
 app.get('/usuarios',auth, async function(req,res){
     var queryResult = await query(`select * from usuarios`);
-    console.log(queryResult);
-    res.status(200).json(queryResult);
+    if(queryResult.length == 0){
+        res.status(404).json("No se encontraron usuarios");
+    } else{
+        res.status(200).json(queryResult);
+    }
 })
 
 // POST libro : agregar un nuevo libro a la BD
 app.post('/libros',auth, async function(req, res){
-    if(req.body.cantidad < 0){
+    if(!req.body.titulo){
+        res.status(400).json("Debe completar el campo titulo");
+    }
+    else if(!req.body.cantidad){
+        res.status(400).json("Debe completar el campo cantidad");
+    }
+    else if(req.body.cantidad <= 0){
         res.status(400).json("Debe ingresar una cantidad mayor a cero");
     }
     else if(!Number.isInteger(req.body.cantidad)){
         res.status(400).json("Debe ingresar un entero");
     }   
-    else if(req.body.cantidad == null){
-        res.status(400).json("Debe completar el campo cantidad");
-    }
-    else if(req.body.titulo == null){
-        res.status(400).json("Debe completar el campo titulo");
-    }
     else{
         const queryStr = `insert into libros (id_libro, titulo, cantidad) values (?, ?, ?)`;
         var queryLibros = await query(queryStr, [getNewID(), req.body.titulo, req.body.cantidad]);
@@ -137,12 +151,21 @@ app.post('/libros',auth, async function(req, res){
 
 // PUT libro : actualizo la cantidad de ejemplares de un libro
 app.put('/libros/:idLibro',auth, async function(req,res){
-    const queryExisteLibro = `select * from libros where id_libro = (?)`;
-    var queryResultExisteLibro = await query(queryExisteLibro, req.params.idLibro);
-    
-    if (req.body.cantidad >= 0){
+    if(req.body.cantidad == null){
+        res.status(400).json("Debe completarse cantidad");
+    } 
+    else if(req.body.cantidad < 0){
+        res.status(400).json("La cantidad debe ser mayor a cero");
+    }
+    else if(!Number.isInteger(req.body.cantidad)){
+        res.status(400).json("La cantidad debe ser un numero entero");
+    }
+    else{
+        const queryExisteLibro = `select * from libros where id_libro = (?)`;
+        var queryResultExisteLibro = await query(queryExisteLibro, req.params.idLibro);
+        
         if (queryResultExisteLibro.length <= 0){
-          res.status(404).json("Libro not found");
+            res.status(404).json("Libro no encontrado");
         } 
         else {
             const queryCantPrestada = `select count(*) as cant_prestada from prestamos where id_libro = (?)`;
@@ -157,17 +180,12 @@ app.put('/libros/:idLibro',auth, async function(req,res){
             }
         } 
     }
-    else{
-      res.status(400).json("Se espera un entero mayor a 0");
-    }
 })
 
 // DELETE libro : elimino un libro de la BD
 app.delete('/libros/:idLibro',auth, async function(req, res){
     const queryLibros = `select * from libros where id_libro = (?)`;
     var queryResultLibros = await query(queryLibros, req.params.idLibro);
-
-    console.log(queryResultLibros);
 
     if(queryResultLibros.length <= 0){
         res.status(404).json("Libro no encontrado");
@@ -192,7 +210,7 @@ app.get('/libros/:idLibro',auth, async function(req,res){
     const queryStr = `select cantidad from libros where id_libro = (?)`;
     var queryResult = await query(queryStr,req.params.idLibro);
     if (queryResult.length <= 0){
-      res.status(404).json("Libro not found");
+      res.status(404).json("Libro no encontrado");
     } 
     else {
         const queryCantPrestada = `select count(*) as cant_prestada from prestamos where id_libro = (?)`;
@@ -244,27 +262,9 @@ app.delete('/prestamos/:idPrestamo',auth, async function(req, res){
 // GET libros : listar todos los libros
 app.get('/libros', async function(req,res){
     var queryResult = await query(`select * from libros`);
-    console.log(queryResult);
-    res.status(200).json(queryResult);
-})
-
-// GET prestamos : listar todos los prestamos (admin)
-app.get('/prestamos', async function(req,res){
-    if(req.session.rol == "s" || req.session.rol == "S"){
-        const queryStr = `select * from prestamos where id_socio = (?)`;
-        var queryResult = await query(queryStr,req.session.id_socio);
-        console.log(queryResult);
-        res.status(200).json(queryResult);
-    }
-    else{
-        var queryResult = await query(`select id_prestamo, libros.id_libro, usuarios.id, libros.titulo, usuarios.nombre, fecha_vto
-                                        from prestamos join usuarios join libros 
-                                        where prestamos.id_socio = usuarios.id && 
-                                              prestamos.id_libro = libros.id_libro`);
-        for(p of queryResult){
-            p.fecha_vto = (new Date(p.fecha_vto)).toLocaleDateString('eu-ES');
-        }
-        console.log(queryResult);
+    if (queryResult.length == 0){
+        res.status(404).json("No hay libros disponibles");
+    } else{
         res.status(200).json(queryResult);
     }
 })
@@ -273,13 +273,50 @@ app.get('/prestamos', async function(req,res){
 // ## Autorizado solo para el socio ## //
 /////////////////////////////////////////
 
+// GET prestamos : si eres socio listas todos tus prestamos
+// y si eres admin lista todos lo prestamos de la biblioteca
+app.get('/prestamos', async function(req,res){
+    if(req.session.rol == "s" || req.session.rol == "S"){
+        const queryStr = `select * from prestamos where id_socio = (?)`;
+        var queryResult = await query(queryStr,req.session.id_user); 
+        if (queryResult.length == 0){
+            res.status(404).json("No tienes prestamos");
+        } else{
+            res.status(200).json(queryResult);
+        }
+    }
+    else{
+        var queryResult = await query(`select id_prestamo, libros.id_libro, prestamos.id_socio, libros.titulo, usuarios.nombre, fecha_vto
+                                        from prestamos join usuarios join libros 
+                                        where prestamos.id_socio = usuarios.id && 
+                                              prestamos.id_libro = libros.id_libro`);
+        if (queryResult.length == 0){
+            res.status(404).json("No hay prestamos");
+        } else{
+            for(p of queryResult){
+                p.fecha_vto = (new Date(p.fecha_vto)).toLocaleDateString('eu-ES');
+            }
+            res.status(200).json(queryResult);
+        }
+    }
+})
+
 // POST prestamo : agregar un prestamo
 app.post('/prestamos', async function(req, res){
-    if(req.session.rol == "s" || req.session.rol == "S"){
+    if(!req.body.idLibro){
+        res.status(400).json("Debe completar la id del libro");
+    }
+    else if(!req.body.cantidadDias){
+        res.status(400).json("Debe completar la cantidad de dias de prestamo");
+    }
+    else if(!Number.isInteger(req.body.cantidadDias)){
+        res.status(400).json("Cantidad de dias debe ser un numero entero");
+    }
+    else if(req.session.rol == "s" || req.session.rol == "S"){
         const queryLibro = `select * from libros where id_libro = (?)`;
         var queryidLibro = await query(queryLibro,req.body.idLibro);
 
-        var idSocio = req.session.id_socio;
+        var idSocio = req.session.id_user;
 
         const queryCantidad = `select count(*) cant from prestamos where id_libro = (?)`;
         var queryCantidadLibros = await query(queryCantidad,req.body.idLibro);
@@ -307,7 +344,7 @@ app.post('/prestamos', async function(req, res){
             else{
                 const queryStr = `insert into prestamos (id_prestamo, id_libro, id_socio, fecha_vto) values (?, ?, ?, ?)`;
                 var queryResult = await query(queryStr, [getNewID(), req.body.idLibro, idSocio, obtenerFechaVto(req.body.cantidadDias)]);
-                console.log(queryResult);
+    
                 res.status(201).json("Se agrego el prestamo correctamente");
             }
         }
@@ -317,56 +354,16 @@ app.post('/prestamos', async function(req, res){
     }
 })
 
-// REQ Obtener libros prestados a un socio
-// app.get('/prestamos/socios/:idSocio', async function(req, res){
-//     const querySocio = `select * from usuarios where id = (?)`;
-//     var queryidSocio = await query(querySocio,req.params.idSocio);
-
-//     if(queryidSocio.length<=0){
-//         res.status(404).json("Socio no encontrado");
-//     }
-//     else{
-//         const queryPrestamos = `select * from prestamos where id = (?)`;
-//         var queryPrestamosSocios = await query(queryPrestamos,req.params.idSocio);
-//         if(queryPrestamosSocios.length <= 0){
-//             res.status(404).json("No se encontraron libros prestado al socio");
-//         }
-//         else{
-//             res.status(200).json(queryPrestamosSocios);
-//         }
-//     }
-// })
-// app.get('/prestamos/socios/user', async function (req, res) {
-//     const querySocio = `select * from usuarios where id = (?)`;
-//     console.log("sesion id " + req.session.id_user)
-//     var queryidSocio = await query(querySocio, req.session.id_user);
-
-//     if (queryidSocio.length <= 0) {
-//         res.status(404).json("Socio no encontrado");
-//     }
-//     else {
-//         const queryPrestamos = `select * from prestamos inner join libros where id_socio = (?)`;
-//         var queryPrestamosSocios = await query(queryPrestamos, req.session.id_user);
-//         if (queryPrestamosSocios.length <= 0) {
-//             res.status(404).json("No se encontraron libros prestado al socio");
-//         }
-//         else {
-//             res.status(200).json(queryPrestamosSocios);
-//         }
-//     }
-// })
-
-app.get('/prestamos/vencidos', async function (req, res) {
+app.get('/prestamos_vencidos', auth, async function (req, res) {
     var fechaMiliseg = (new Date()).getTime();
     const queryPrestamos = `select * from prestamos where fecha_vto < ?`;
     var queryPrestamosGeneral = await query(queryPrestamos, [fechaMiliseg]);
-    if (queryPrestamosGeneral <= 0) {
+    if (queryPrestamosGeneral.length <= 0) {
         res.status(400).json("No hay prestamos vencidos");
     } else {
         res.status(200).json(queryPrestamosGeneral);
     }
 })
-
 
 ////////////////////////////////
 // ## Funciones auxiliares ## //
